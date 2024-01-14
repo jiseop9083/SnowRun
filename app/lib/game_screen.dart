9ce6c846d6df2164e912fd6e0c4d155e324c44ce
jiseop9083 <sinphi03@gmail.com>
2024-01-14 15:11:36 +0900

    temp

--- app/lib/game_screen.dart
+++ app/lib/game_screen.dart
@@ -8,7 +8,7 @@ import 'package:app/levels/level.dart';
 class SnowManGame extends FlameGame with TapCallbacks {
   late final CameraComponent cam;
 
-  //final world = Level();
+  final world = Level();
 
   late Player player;
 
@@ -20,7 +20,7 @@ class SnowManGame extends FlameGame with TapCallbacks {
     player = Player(
       position: Vector2(size.x * 0.75, size.y - 20),
     );
-    addAll([cam, player]); // DOTO: insert world, cam
+    addAll([cam, world, player]); // DOTO: insert world, cam
     return super.onLoad();
   }
 
@@ -37,19 +37,19 @@ class SnowManGame extends FlameGame with TapCallbacks {
     }
   }
 
-  @override
-  void render(Canvas canvas) {
-    // canvas.drawRect(
-    //   Rect.fromLTWH(0, 0, size.x, size.y),
-    //   Paint()..color = Colors.blue,
-    // );
-    super.render(canvas);
-    cam.render(canvas);
-    player.render(canvas);
+  // @override
+  // void render(Canvas canvas) {
+  //   // canvas.drawRect(
+  //   //   Rect.fromLTWH(0, 0, size.x, size.y),
+  //   //   Paint()..color = Colors.blue,
+  //   // );
+  //   super.render(canvas);
+  //   cam.render(canvas);
+  //   player.render(canvas);
 
-    // 다른 그래픽 요소들을 그릴 수 있습니다.
-    // 예: drawRect, drawCircle 등
-  }
+  //   // 다른 그래픽 요소들을 그릴 수 있습니다.
+  //   // 예: drawRect, drawCircle 등
+  // }
 }
 
 class Player extends RectangleComponent {
