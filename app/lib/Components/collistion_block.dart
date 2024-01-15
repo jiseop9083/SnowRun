da59239bd25afa2e786017f3c179bea9d54c9316
jiseop9083 <sinphi03@gmail.com>
2024-01-15 21:25:16 +0900

    ✨ add slope block

--- app/lib/Components/collistion_block.dart
+++ app/lib/Components/collistion_block.dart
@@ -2,10 +2,14 @@ import 'package:flame/components.dart';
 
 class ColliisionBlock extends PositionComponent {
   bool isPlatform;
+  bool isSlope;
   ColliisionBlock({
     position,
     size,
     this.isPlatform = false,
+    this.isSlope = false,
+    leftTop = 16,
+    rightTop = 16,
   }) : super(position: position, size: size) {
     debugMode = true; // debugMode
   }
