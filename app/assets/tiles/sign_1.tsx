81fea2ddf25da89bc45bec01403df74de5fe7e15
Jian Lee <jalee0201@kaist.ac.kr>
2024-01-16 18:45:33 +0900

    commit

---
+++ app/assets/tiles/sign_1.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.10.2" name="sign_1" tilewidth="64" tileheight="64" tilecount="6" columns="2">
+ <image source="../images/sign_1.png" width="182" height="196"/>
+</tileset>
