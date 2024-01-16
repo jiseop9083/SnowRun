3ef68ced5a8658318a3f24f52ab56776cabbee5c
Jian Lee <jalee0201@kaist.ac.kr>
2024-01-16 20:22:28 +0900

    commit

---
+++ app/assets/tiles/crystal.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.10.2" name="crystal" tilewidth="64" tileheight="64" tilecount="6" columns="3">
+ <image source="../images/crystal.png" width="203" height="163"/>
+</tileset>
