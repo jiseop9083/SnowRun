51780ddc3535f9b41a2ef24923e48622646f7fb0
Jian Lee <jalee0201@kaist.ac.kr>
2024-01-17 16:28:36 +0900

    commit: login, signup

--- app/lib/functional.dart
+++ app/lib/functional.dart
@@ -1,10 +1,58 @@
 import 'package:http/http.dart' as http;
 import 'dart:convert';
-import 'dart:io';
-import 'package:file_picker/file_picker.dart';
-import 'package:path_provider/path_provider.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 
 // API의 URL. 실제 URL로 대체해야 합니다.
-const String apiUrl = "http://172.10.7.69";
+const String apiUrl = "http://172.10.7.69:80";
 
+Future<bool> checkNewUser(String nickname) async {
+  final response = await http.get(Uri.parse('$apiUrl/new_user/?user_id=$nickname'));
+  return json.decode(response.body);
+}
+
+Future<int> createUser(String nickname, String password) async {
+  final response = await http.post(
+    Uri.parse('$apiUrl/users/'),
+    headers: {'Content-Type': 'application/json'},
+    body: json.encode({'user_id': nickname, 'password': password}),
+  );
+  // HTTP 요청이 성공적으로 처리되었습니다.
+    var data = json.decode(response.body);
+    return data['user_index'];
+}
+
+Future<dynamic> loginUser(String nickname, String password) async {
+  final response = await http.get(
+    Uri.parse('$apiUrl/login/?user_id=$nickname&password=$password'),
+  );
+  var data = json.decode(response.body);
+  if (data == false) {
+    return false;
+  } else {
+    return data['user_index'];
+  }
+}
+
+Future<void> saveUserIndex(int userIndex) async {
+  final SharedPreferences prefs = await SharedPreferences.getInstance();
+  await prefs.setInt('userIndex', userIndex);
+}
+
+Future<int?> getUserIndex() async {
+  final SharedPreferences prefs = await SharedPreferences.getInstance();
+  return prefs.getInt('userIndex');
+}
+
+Future<Map<String, dynamic>> updateScore(int userIndex, int score) async {
+  final response = await http.get(
+    Uri.parse('$apiUrl/update-score/?user_index=$userIndex&score=$score'),
+  );
+  return json.decode(response.body);
+}
+
+Future<List<dynamic>> getScores() async {
+  final response = await http.get(Uri.parse('$apiUrl/get-scores/'));
+  // 서버로부터 정상적인 응답을 받았을 때
+  List<dynamic> scores = json.decode(response.body);
+  return scores;
+}
--- app/lib/login.dart
+++ app/lib/login.dart
@@ -2,6 +2,7 @@ import 'package:app/signUp.dart';
 import 'package:flame/game.dart';
 import 'package:flutter/material.dart';
 import 'package:app/game_screen.dart';
+import '../functional.dart';
 
 
 class Login extends StatefulWidget {
@@ -12,13 +13,44 @@ class Login extends StatefulWidget {
 class _LoginState extends State<Login> {
   final TextEditingController _nicknameController = TextEditingController();
   final TextEditingController _passwordController = TextEditingController();
+  final _scaffoldKey = GlobalKey<ScaffoldState>(); // 스낵바를 위한 키
+
+  void _login() async {
+    String nickname = _nicknameController.text;
+    String password = _passwordController.text;
+
+    dynamic result = await loginUser(nickname, password);
+
+    if (result == false) {
+      // 로그인 실패 메시지 표시
+      print("snackbar");
+      ScaffoldMessenger.of(context).showSnackBar(
+        SnackBar(
+          content: Text('존재하지 않는 아이디 혹은 비번입니다.'),
+          duration: Duration(seconds: 2),
+        ),
+      );
+    } else {
+      print("no snackbar");
+      // user_index를 SharedPreferences에 저장
+      await saveUserIndex(result);
+
+      // 로그인 성공 시 GameWidget으로 이동
+      Navigator.push(
+        context,
+        MaterialPageRoute(
+          builder: (context) => GameWidget(game: SnowManGame(tileSize: 64)),
+        ),
+      );
+    }
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        appBar: AppBar(
+      appBar: AppBar(
         title: Text('로그인'),
-        ),
+      ),
       body: Padding(
         padding: EdgeInsets.all(16.0),
         child: Column(
@@ -45,15 +77,7 @@ class _LoginState extends State<Login> {
             SizedBox(height: 20), // 간격
             // 로그인 버튼
             ElevatedButton(
-              onPressed: () {
-              // 로그인 로직 (여기서 입력 값을 사용)
-                Navigator.push(
-                  context,
-                  MaterialPageRoute(
-                    builder: (context) => GameWidget(game: SnowManGame(tileSize: 64)),
-                  ),
-                );
-              },
+              onPressed: _login,
               child: Text('로그인'),
             ),
             SizedBox(height: 10), // 간격
@@ -74,3 +98,4 @@ class _LoginState extends State<Login> {
       ),
     );
   }
+}
